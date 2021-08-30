44751720eb67763cfd4fab5122a0b940a2e8519d
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-30 14:36:50 -0300

    comentado e finalizado

--- Exer 11.py
+++ Exer 11.py
@@ -1,3 +1,4 @@
+#Questão 11 - converter de m/s para km/h
 VeloM = float(input("Digite a velocidade desejada(Em M/S) para que ela seja convertida (Em Km/H) "))
 VeloK = (VeloM*3.6)
 print("A velocidade já convertida é: {:.2f} m/s".format(VeloK))
