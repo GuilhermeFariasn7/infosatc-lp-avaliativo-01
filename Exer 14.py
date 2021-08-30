dbdc26ad0515a045c28e74f7401ac7895ddeaa37
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-30 14:36:54 -0300

    comentado e finalizado

--- Exer 14.py
+++ Exer 14.py
@@ -1,3 +1,4 @@
-G = float(input("Digite um angulo em graus: "))
+##Questão 14 - converter um ângulo para radianos
+G = float(input("Digite um ângulo em graus: "))
 R = ((G * 3.14)/180)
 print("O angulo {} em radianos ficarah: {:.4f}".format(G,R))
