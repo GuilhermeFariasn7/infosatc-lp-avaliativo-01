26c81798dcfa2865cca8205167a5b192a1e03bee
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-30 13:44:40 -0300

    Create Exer 11.py

---
+++ Exer 11.py
@@ -0,0 +1,3 @@
+VeloM = float(input("Digite a velocidade desejada(Em M/S) para que ela seja convertida (Em Km/H) "))
+VeloK = (VeloM*3.6)
+print("A velocidade já convertida é: {:.2f} m/s".format(VeloK))
