f5e3f1978a744d4f26160b89ee7a8b1f5fc53feb
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-30 14:20:53 -0300

    Update Exer 28.py

--- Exer 28.py
+++ Exer 28.py
@@ -1,7 +1,6 @@
-print('\033[4;33mDigite 4 notas\033[m ')
-n1 = float(input("Digite a primeira nota: "))
-n2 = float(input("Digite a segunda nota: "))
-n3 = float(input("Digite a terceira nota: "))
-n4 = float(input("Digite a quarta nota: "))
+print('\033[4;33mDigite 3 valores!\033[m ')
+n1 = float(input("Digite o primeiro valor: "))
+n2 = float(input("Digite o segundo valor: "))
+n3 = float(input("Digite o terceiro valor: "))
 
-print("Sua média será: {:.1f}".format((n1+n2+n3+n4)/4))
+print("A soma dos quadrados dos três valores desejados será: {}".format((n1**2)+(n2**2)+(n3**2)))
