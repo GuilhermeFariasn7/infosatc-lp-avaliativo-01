ad13123eebe6b18b4ae519d04640f37a55be4c6c
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-08-30 14:29:41 -0300

    Create Exer 30.py

---
+++ Exer 30.py
@@ -0,0 +1,7 @@
+C = float(input("Digite a cotação do dólar: "))
+R = float(input("Digite o valor desejado a ser convertido em dolar: "))
+
+D = (R/C)
+
+print("O valor convertido de reais para dolar: {:.2f}".format(D))
+
